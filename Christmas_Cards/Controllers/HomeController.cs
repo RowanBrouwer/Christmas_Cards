b8812acb94e3c3bdbb45db63e7856ee09dfb1d3e
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-12-10 13:27:18 +0100

    added controler (model still not valid)

--- Christmas_Cards/Controllers/HomeController.cs
+++ Christmas_Cards/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Christmas_Cards.DAL;
+
 
 namespace Christmas_Cards.Controllers
 {
@@ -13,9 +15,13 @@ namespace Christmas_Cards.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDBContext db;
+        
+
+        public HomeController(ILogger<HomeController> logger, AppDBContext db) 
         {
             _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
@@ -27,6 +33,25 @@ namespace Christmas_Cards.Controllers
         {
             return View();
         }
+
+        //Include = "Id,FirstName,InFix,LastName,Wage"
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind()] CardModel cardModel)
+        {
+            cardModel.Image = db.Images.FirstOrDefault(i => i.Id == 1);
+            if (ModelState.IsValid)
+            { 
+                
+                db.Cards.Add(cardModel);
+                db.SaveChanges();
+                return RedirectToAction("Privacy");
+            }
+
+            return View("Index");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
