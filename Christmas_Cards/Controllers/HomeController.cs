31ee1bd4439706032dc06ea4b244c73f30790557
Rowan Brouwer <brouwerrowan@gmail.com>
2020-12-14 10:58:42 +0100

    PDFConv updated

--- Christmas_Cards/Controllers/HomeController.cs
+++ Christmas_Cards/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
 ﻿using Christmas_Cards.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Christmas_Cards.Controllers
@@ -32,6 +37,50 @@ namespace Christmas_Cards.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void ConvertToPdf()
+        {
+            PdfDocument document = new PdfDocument();
+
+            PdfPage page = document.Pages.Add();
+
+            PdfBitmap image = new PdfBitmap($"{}");
+
+            PdfGraphicsState state = page.Graphics.Save();
+
+            page.Graphics.SetTransparency(0.2f);
+
+            page.Graphics.DrawImage(image, new PointF(0,0), new SizeF(page.GetClientSize().Width, page.GetClientSize().Height));
+
+            page.Graphics.Restore(state);
+
+            PdfFont font = new PdfTrueTypeFont($"{}");
+
+            PdfSolidBrush brush = new PdfSolidBrush($"{}");
+
+            page.Graphics.DrawString($"{}", font, brush, new PointF(0,0));
+
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            document.Close(true);
+
+            stream.Position = 0;
+
+            Attachment file = new Attachment(stream, $"{}", $"{}");
+            using (SmtpClient smtp = new SmtpClient($"{}"))
+            {
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("X-Mas-Cards@gmail.com");
+                message.To.Add("");
+                message.Subject = "Some one send you an X-Mas Card!";
+                message.Attachments.Add(file);
+                message.IsBodyHtml = false;
+                smtp.Send(message);
+            }
+            
         }
     }
 }
